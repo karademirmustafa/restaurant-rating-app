dabd52806cacff238fa8f11bdfa733f42dae9c77
karademirmustafa <d3n3m32011@hotmail.com>
2023-10-20 13:54:01 +0300

    Added module rating and restaurant service

--- server/src/rating/rating.module.ts
+++ server/src/rating/rating.module.ts
@@ -3,10 +3,12 @@ import { RatingController } from './rating.controller';
 import { RatingService } from './rating.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Rating, RatingSchema } from './rating.entity';
+import { Restaurant, RestaurantSchema } from 'src/restaurants/restaurants.entity';
+import { RestaurantsService } from 'src/restaurants/restaurants.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name:Rating.name,schema:RatingSchema}])],
+  imports: [MongooseModule.forFeature([{name:Rating.name,schema:RatingSchema}]),MongooseModule.forFeature([{ name: Restaurant.name, schema: RestaurantSchema }])],
   controllers: [RatingController],
-  providers: [RatingService]
+  providers: [RatingService,RestaurantsService]
 })
 export class RatingModule {}
--- server/src/restaurants/restaurants.module.ts
+++ server/src/restaurants/restaurants.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Restaurant,RestaurantSchema } from './restaurants.entity';
+import { Restaurant, RestaurantSchema } from './restaurants.entity';
+import { Rating, RatingSchema } from 'src/rating/rating.entity';
+import { RatingService } from 'src/rating/rating.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name:Restaurant.name,schema:RestaurantSchema}])],
+  imports: [MongooseModule.forFeature([{ name: Restaurant.name, schema: RestaurantSchema }]), MongooseModule.forFeature([{ name: Rating.name, schema: RatingSchema }])],
   controllers: [RestaurantsController],
-  providers: [RestaurantsService]
+  providers: [RestaurantsService, RatingService]
 })
-export class RestaurantsModule {}
+export class RestaurantsModule { }
