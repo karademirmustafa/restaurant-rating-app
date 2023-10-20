080468362b78df9a0a0a622d1979fe0a0903b475
karademirmustafa <d3n3m32011@hotmail.com>
2023-10-20 22:20:18 +0300

    Added global error handling

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exception-filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   // Validation
   app.useGlobalPipes(new ValidationPipe())
-  const configService = app.get(ConfigService);
   const port = configService.get('port');
+  // Error Handling
+  const logger = new Logger(); // Create a Logger instance
+  app.useGlobalFilters(new HttpExceptionFilter(logger));
   // react default port
   app.enableCors({ origin: 'http://localhost:3000', credentials: true })
   await app.listen(port);
