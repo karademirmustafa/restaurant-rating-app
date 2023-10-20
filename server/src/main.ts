7f1cd700c9bc2471438bf475e7c08af665dd01ae
karademirmustafa <d3n3m32011@hotmail.com>
2023-10-20 11:18:51 +0300

    added validation global error

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Validation
+  app.useGlobalPipes(new ValidationPipe())
   const configService = app.get(ConfigService);
-
   const port = configService.get('port');
+
   await app.listen(port);
 }
 bootstrap();
--- server/src/restaurants/dtos/create-restaurant.dto.ts
+++ server/src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,6 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
 
 export class CreateRestaurantDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     name: string;
+
+    @IsString()
     description: string;
+
+    @IsNumber()
+    @IsNotEmpty()
     rating: number;
   }
