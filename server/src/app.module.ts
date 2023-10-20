81eb4adb2595532cb59585e8eb81b95b23b9b955
karademirmustafa <d3n3m32011@hotmail.com>
2023-10-20 10:57:41 +0300

    add restaurant endpoints crud structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { config } from './config';
 import { MongooseModule } from "@nestjs/mongoose";
+import { RestaurantsModule } from './restaurants/restaurants.module';
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true,load:[config]}),MongooseModule.forRoot(config().mongo_uri)],
+  imports: [ConfigModule.forRoot({isGlobal:true,load:[config]}),MongooseModule.forRoot(config().mongo_uri), RestaurantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
