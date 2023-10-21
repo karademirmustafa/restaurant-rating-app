28c82ec528f6d8dde8232e83772a7e2a694dd6b8
karademirmustafa <d3n3m32011@hotmail.com>
2023-10-21 17:32:11 +0300

    Added restaurants lists and restaurant add, pagination,sorting

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import RestaurantTable from '../components/Restaurant/Table';
+import { RestaurantI } from '../types';
+import restaurantService from '../services/RestaurantService';
+import RestaurantForm from '../components/Restaurant/Form';
+
+
+const Home: React.FC = () => {
+    const [restaurants, setRestaurants] = useState<RestaurantI[]>([]);
+    const [pagination, setPagination] = useState<{ currentPage: number, totalItems: number, totalPages: number }>();
+    const [sorting, setSorting] = useState<{ order: 'ASC' | 'DESC' }>({
+        order: 'ASC',      // Default sorting order
+    });
+
+    const fetchRestaurantData = async (page: number, sortOrder: 'ASC' | 'DESC') => {
+        const rest = await restaurantService.getRestaurants(page, sortOrder);
+        setPagination({ currentPage: rest.page, totalItems: rest.totalItems, totalPages: rest.totalPages });
+        setRestaurants(rest.restaurants);
+    };
+
+    useEffect(() => {
+        (async () => {
+            fetchRestaurantData(1, sorting.order);
+        })()
+    }, []);
+
+    const handlePageChange = async (newPage: number) => {
+        fetchRestaurantData(newPage, sorting.order);
+    };
+
+    const handleSortChange = async () => {
+        const newSortOrder = sorting.order === 'ASC' ? 'DESC' : 'ASC';
+        setSorting({ order: newSortOrder });
+        fetchRestaurantData(1, newSortOrder);
+    };
+   
+
+
+    return (
+        <>
+            <RestaurantTable
+                restaurants={restaurants}
+                pagination={pagination || { currentPage: 1, totalItems: 0, totalPages: 1 }}
+                onPageChange={handlePageChange}
+                onSortChange={handleSortChange}
+                currentSort={sorting}
+            />
+      
+        </>
+    );
+};
+export default Home;
