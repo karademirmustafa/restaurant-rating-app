ef23e19728a7e8c7393283db1662dd288b9ab92c
karademirmustafa <d3n3m32011@hotmail.com>
2023-10-21 17:34:47 +0300

    info page description update

--- client/src/pages/Info.tsx
+++ client/src/pages/Info.tsx
@@ -18,7 +18,7 @@ const CardLink: React.FC<CardLinkProps> = ({ to, text,description, iconClass }) 
                     <p className="card-text">{description}</p>
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="btn-group">
-                            <Link to={to} className="btn btn-sm btn-outline-secondary">Go</Link>
+                            <Link to={to} target="_blank" className="btn btn-sm btn-outline-secondary">Go</Link>
                         </div>
                     </div>
                 </div>
@@ -34,8 +34,8 @@ const Info: React.FC = () => {
                 <div className="row">
                     <CardLink to="/restaurants" text="Restaurants" description='View all restaurants sorted by their rating.You can rate and update the restaurant, 10 restaurants are listed on each page and pagination is enabled.' iconClass="fas fa-utensils" />
                     <CardLink to="/restaurants" text="Add Restaurant" description='If you are logged in, you can create a new restaurant' iconClass="fas fa-plus" />
-                    <CardLink to="/login" text="Login" description='You can go to sign in.' iconClass="fas fa-sign-in-alt" />
-                    <CardLink to="/register" text="Register" description='You can go to sign up.' iconClass="fas fa-user-plus" />
+                    <CardLink to="/login" text="Login" description='You can go to sign in. assigns you to the restaurant page if you are logged in' iconClass="fas fa-sign-in-alt" />
+                    <CardLink to="/register" text="Register" description='You can go to sign up. assigns you to the restaurant page if you are logged in' iconClass="fas fa-user-plus" />
                     <CardLink to="http://localhost:4000/api-docs" text="Swagger Api Docs" description='You can view the api documentation.' iconClass="fas fa-user-plus" />
                 </div>
             </div>
